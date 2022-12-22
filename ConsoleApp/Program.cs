d1defb2e0d9d1c2d48a9049355f9ade934468562
Your Name <areshetnikov@sfedu.ru>
2022-12-22 22:21:51 +0300

    fact fix

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,14 +6,14 @@ namespace ConsoleApp
     {
         public static int Factorial(int n)
         {
-            if (n == 1) return 1;
+            if (n == 0 || n == 1) return 1;
 
             return n * Factorial(n - 1);
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(0));
         }
     }
 }
